56f768a92fcc6ce1db4850aa7ec26c86cc40df12
William Gan <wjgan@berkeley.edu>
2017-03-15 23:55:55 -0700

    Decision Tree code

--- day11/calc_gini.py
+++ day11/calc_gini.py
@@ -1,18 +1,26 @@
 import numpy as np
 """
 In the below function you will be implementing a function
-to calculate the gini impurity for a given collection of labels 
+to calculate the gini impurity for a given collection of labels
 below are some examples to test how your function behaves
 """
 def gini_index(classes):
-	raise Exception('Not implemented')
-
+	counts = {}
+	for x in classes:
+		if x not in counts:
+			counts[x] = 1
+		else:
+			counts[x] += 1
+	sum = 0
+	for c in counts.values():
+		f = c/len(classes)
+		sum += f*f
+	return 1-sum
 
 """
-Uncomment these tests to see how 
+Uncomment these tests to see how
 your function behaves
 
 """
-#gini_index([0, 0, 0, 0,0, 0]) -> 0
-#gini_index([0, 1, 0, 1, 0, 1]) -> 0.5
-
+print(gini_index([0, 0, 0, 0,0, 0]))
+print(gini_index([0, 1, 0, 1, 0, 1]))
